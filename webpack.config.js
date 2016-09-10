a719340d35a6e4df2cab8737b190ab3646a77f60
Jamie Albinson <jalbinson@learningobjects.com>
2016-09-10 15:49:29 -0400

    other stuff

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var srcPath = path.join(__dirname, 'src');
+var buildPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    context: srcPath,
+    entry: path.join(srcPath, 'js', 'client.js'),
+    output: {
+        path: buildPath,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader" }
+        ]
+    },
+    devtool: '#cheap-eval-source-map',
+};
